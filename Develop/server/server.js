b212d8dac05b0217f051c79f6afc27fcf7843ede
Andrew Breytenbach <ambreyts15@gmail.com>
2023-06-22 08:47:34 -0400

    updated the server.js file so that Implement the Apollo Server and apply it to the Express server as middleware

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,21 +1,28 @@
 const express = require('express');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schema'); // import typeDefs and resolvers
 const db = require('./config/connection');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Apollo Server setup
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app });
+
+// Middleware and routes
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 app.use(routes);
 
+// Connect to the database and start the server
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
