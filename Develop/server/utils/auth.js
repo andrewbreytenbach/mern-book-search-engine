147095e1515ad66fa6c2f56dcd2da51512c8fce2
Andrew Breytenbach <ambreyts15@gmail.com>
2023-06-22 08:51:43 -0400

    Update the auth middleware function to work with the GraphQL API

--- Develop/server/utils/auth.js
+++ Develop/server/utils/auth.js
@@ -1,39 +1,40 @@
 const jwt = require('jsonwebtoken');
 
-// set token secret and expiration date
+// Set token secret and expiration date
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+  // Middleware function for authentication
+  authMiddleware: function (context) {
+    const { req } = context;
+    let token = req.headers.authorization || '';
 
-    // ["Bearer", "<tokenvalue>"]
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+    if (token.startsWith('Bearer ')) {
+      // Remove 'Bearer ' from the token
+      token = token.substring(7, token.length);
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      // If no token is present, throw an error
+      throw new Error('You have no token!');
     }
 
-    // verify token and get user data out of it
     try {
+      // Verify the token and extract the user data
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
+      context.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      throw new Error('Invalid token!');
     }
-
-    // send to next endpoint
-    next();
   },
+
+  // Function to sign a JWT token
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
-
+    
+    // Create a new JWT token with the payload, secret, and expiration time
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
