89968f919af7fe26d8b13a5a889cfaa2d85997fd
Andrew Breytenbach <117181767+andrewbreytenbach@users.noreply.github.com>
2023-06-23 09:12:04 -0400

    made some minor modifications to the Book.js file

--- Develop/server/models/Book.js
+++ Develop/server/models/Book.js
@@ -1,7 +1,12 @@
-const { Schema } = require('mongoose');
+const mongoose = require('mongoose');
 
-// This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
+const { Schema } = mongoose;
+
 const bookSchema = new Schema({
+  bookId: {
+    type: String,
+    required: true,
+  },
   authors: [
     {
       type: String,
@@ -11,21 +16,20 @@ const bookSchema = new Schema({
     type: String,
     required: true,
   },
-  // saved book id from GoogleBooks
-  bookId: {
+  title: {
     type: String,
     required: true,
   },
   image: {
     type: String,
+    required: true,
   },
   link: {
-    type: String,
-  },
-  title: {
     type: String,
     required: true,
   },
 });
 
-module.exports = bookSchema;
+const Book = mongoose.model('Book', bookSchema);
+
+module.exports = Book;
