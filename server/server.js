2950f236df1841b2422c69d659cad575f3d9420e
Andrew Breytenbach <117181767+andrewbreytenbach@users.noreply.github.com>
2023-06-27 09:00:01 -0400

    working through heroku deployment issues

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
-const { typeDefs, resolvers } = require('./schema'); // import typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas'); // import typeDefs and resolvers
 const db = require('./config/connection');
 const routes = require('./routes');
 
